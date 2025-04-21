56ba3689cba82956ca0ae118b668b46ade7ca704
FrancescoDuluta <frenkdarcud18@gmail.com>
2025-04-21 22:36:37 +0200

    Correzione errore di refuso nella documentazione (mancavano i not null nella parte sql delle create table).

--- database/database.sql
+++ database/database.sql
@@ -4,36 +4,36 @@ USE Rollo;
 
 CREATE TABLE Utenti (
     id_Utente MEDIUMINT PRIMARY KEY,
-    nome VARCHAR(100),
-    cognome VARCHAR(100),
-    email VARCHAR(150) UNIQUE,
-    password VARCHAR(100),
-    num_token MEDIUMINT
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    num_token MEDIUMINT DEFAULT 100  NOT NULL
 );
 
 CREATE TABLE Sedi (
     id_Sede SMALLINT PRIMARY KEY,
     nome VARCHAR(100),
-    comune VARCHAR(100),
-    cap DECIMAL(5,0),
-    via VARCHAR(100),
+    comune VARCHAR(100) NOT NULL,
+    cap DECIMAL(5,0) NOT NULL,
+    via VARCHAR(100) NOT NULL,
     civico SMALLINT,
-    num_Biciclette_Gestite MEDIUMINT,
-    telefono VARCHAR(20)
+    num_Biciclette_Gestite MEDIUMINT DEFAULT 0,
+    telefono VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IoT (
     id_Dispositivo MEDIUMINT PRIMARY KEY,
-    latitudine DECIMAL(9,6),
-    longitudine DECIMAL(9,6),
-    data_Installazione DATE,
-    data_Revisione DATE,
+    latitudine DECIMAL(9,6) NOT NULL,
+    longitudine DECIMAL(9,6) NOT NULL,
+    data_Installazione DATE NOT NULL,
+    data_Revisione DATE NOT NULL,
     CHECK (data_Revisione >= data_Installazione)
 );
 
 CREATE TABLE Biciclette (
     id_Bicicletta MEDIUMINT PRIMARY KEY,
-    modello VARCHAR(100),
+    modello VARCHAR(100) NOT NULL,
     scheda_IoT MEDIUMINT,
     sede_appartenenza SMALLINT,
     FOREIGN KEY (scheda_IoT) REFERENCES IoT(id_Dispositivo),
@@ -42,11 +42,11 @@ CREATE TABLE Biciclette (
 
 CREATE TABLE Noleggi (
     id_Noleggio BIGINT PRIMARY KEY,
-    data DATE,
-    ora_Inizio TIME,
+    data DATE DEFAULT NOW(),
+    ora_Inizio TIME NOT NULL,
     ora_Fine TIME,
     distanza_Percorsa FLOAT CHECK (distanza_Percorsa >= 0),
-    prezzo DECIMAL(5,2) CHECK (prezzo >= 0),
+    prezzo DECIMAL(5,0) CHECK (prezzo >= 0),
     utente BIGINT,
     bicicletta MEDIUMINT,
     FOREIGN KEY (utente) REFERENCES Utenti(id_Utente),
@@ -63,7 +63,7 @@ INSERT INTO IoT VALUES (101, 45.4654, 9.1859, '2024-01-01', '2025-01-01');
 
 INSERT INTO Biciclette VALUES (2001, 'Model X', 101, 1);
 
-INSERT INTO Noleggi VALUES (30001, '2025-04-01', '08:00:00', '09:30:00', 12.5, 3.50, 1, 2001);
+INSERT INTO Noleggi VALUES (30001, '2025-04-01', '08:00:00', '09:30:00', 12.5, 3, 1, 2001);
 
 
 -- VISTA: Noleggi con dati utente e bicicletta
