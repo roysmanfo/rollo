ea52680b23267719ce8769950a94013e6a86378a
Roys Manfo <94235711+roysmanfo@users.noreply.github.com>
2025-05-19 07:10:26 +0000

    controllo del numero di righe ritornate dalla query

--- app/backend/biciclette/disponibili.php
+++ app/backend/biciclette/disponibili.php
@@ -5,14 +5,22 @@
         $result = $conn->query("SELECT B.id, B.modello, B.sede_appartenenza, B.stato,
                                 I.latitudine, I.longitudine, I.data_installazione, I.data_revisione
                                 FROM biciclette B
-                                JOIN iot I ON B.iot = I.id;");
-        $biciclette = array();
-        while ($row = $result->fetch_assoc()) {
-            array_push($biciclette, $row);
+                                JOIN iot I ON B.iot = I.id
+                                WHERE B.stato = 1;");
+        if ($result->num_rows > 0){
+            $biciclette = array();
+            while ($row = $result->fetch_assoc()) {
+                array_push($biciclette, $row);
+            }
+            echo json_encode(["message"=> "Ricerca completata", "biciclette" => $biciclette]);
+            exit;
         }
-        echo json_encode(["message"=> "Ricerca completata", "biciclette" => $biciclette]);
+        
+        echo json_encode(["message"=> "Nessuna bilicletta disponibile"]);
     } else {
         http_response_code(405);
         echo json_encode(["error" => "Metodo non consentito."]);
     }
+
+    $conn->close();
 ?>
